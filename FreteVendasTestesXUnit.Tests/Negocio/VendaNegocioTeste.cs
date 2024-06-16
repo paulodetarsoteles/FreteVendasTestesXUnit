38393e14c073a03aa2296fac92ec9db2a100729e
Paulo <paulodetarso.teles@hotmail.com>
2024-06-16 11:08:42 -0300

    Instalação da biblioteca Moq e implantação do Mock de repositório de vendas no construtor

--- FreteVendasTestesXUnit.Tests/Negocio/VendaNegocioTeste.cs
+++ FreteVendasTestesXUnit.Tests/Negocio/VendaNegocioTeste.cs
@@ -1,9 +1,18 @@
-﻿using FreteVendasTestesXUnit.Negocio;
+﻿using FreteVendasTestesXUnit.Repositorio.Interfaces;
+using Moq;
 
 namespace FreteVendasTestesXUnit.Tests.Negocio
 {
     public class VendaNegocioTeste
     {
+        Mock<IVendasRepositorio> _vendasRepositorioMock;
+
+        public VendaNegocioTeste()
+        {
+            _vendasRepositorioMock = new Mock<IVendasRepositorio>();
+        }
+
+        [Fact]
         public void CalcularMediaMensal()
         {
 
